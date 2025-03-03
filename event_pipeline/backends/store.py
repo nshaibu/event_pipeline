228a7f0f9d216e0a37efbe1c24f80cf8d0da8952
nshaibu <nafiushaibu1@gmail.com>
2025-03-03 17:47:01 +0000

    implement scheduling for pipeline events

--- event_pipeline/backends/store.py
+++ event_pipeline/backends/store.py
@@ -16,3 +16,31 @@ class KeyValueStoreBackendBase(abc.ABC):
     @abc.abstractmethod
     def exists(self, key):
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def delete(self, key):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def incr(self, key, amount=1):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def decr(self, key, amount=1):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def lpush(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rpush(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def pop(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def sadd(self, key, value):
+        raise NotImplementedError
