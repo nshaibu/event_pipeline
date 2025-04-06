10e57278e46a90d46b9fc1448e5541d58b3f1977
nshaibu <nafiushaibu1@gmail.com>
2025-04-06 01:52:19 +0000

    added mechanism to save object states

--- event_pipeline/settings.py
+++ event_pipeline/settings.py
@@ -5,13 +5,13 @@ MAX_EVENT_BACKOFF = 100
 MAX_BATCH_PROCESSING_WORKERS = 4
 
 RESULT_BACKEND_CONFIG = {
-    "ENGINE": "event_pipeline.backends.stores.redis_store.RedisStoreBackend",
-    "CONNECTOR_CONFIG": {
-        "host": "localhost",
-        "port": 6379,
-    },
-    "CONNECTION_MODE": "pooled",  # "single" or "pooled" or "auto"
-    "MAX_CONNECTIONS": 10,  # Only used in pooled mode
-    "CONNECTION_TIMEOUT": 30,  # Seconds to wait for connection acquisition
-    "IDLE_TIMEOUT": 300,  # Seconds before closing idle connections
+    "ENGINE": "event_pipeline.backends.stores.inmemory_store.InMemoryKeyValueStoreBackend",
+    # "CONNECTOR_CONFIG": {
+    #     "host": "localhost",
+    #     "port": 6379,
+    # },
+    # "CONNECTION_MODE": "pooled",  # "single" or "pooled" or "auto"
+    # "MAX_CONNECTIONS": 10,  # Only used in pooled mode
+    # "CONNECTION_TIMEOUT": 30,  # Seconds to wait for connection acquisition
+    # "IDLE_TIMEOUT": 300,  # Seconds before closing idle connections
 }
