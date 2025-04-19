4f558d20ea42e8cb8da4d94296813f928a2d7929
nshaibu <nafiushaibu1@gmail.com>
2025-04-19 07:55:21 +0000

    implement xmlrpc and grpc executors and managers

--- event_pipeline/scripts/proto_code_generation.sh
+++ event_pipeline/scripts/proto_code_generation.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-project_root=./event_pipeline/protos
-python_code_dir=./event_pipeline/protos
-proto_script_path=./event_pipeline/protos/task.proto
+project_root=../protos
+python_code_dir=../protos
+proto_script_path=../protos/task.proto
 
-python -m grpc_tools.protoc -I${project_root}\
+python -m grpc_tools.protoc -I${project_root} \
 --python_out=${python_code_dir} \
 --pyi_out=${python_code_dir} \
---grpc_python_out=${python_code_dir}
+--grpc_python_out=${python_code_dir} ${proto_script_path}
