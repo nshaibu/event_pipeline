b8a0f713874b0f9fcbb77cabfa373eb9a097e9dd
lateo <lateo@lateos-MacBook-Air.local>
2025-08-16 23:55:41 +0100

    add empty pipeline wrapper file

---
+++ event_pipeline/pipeline_wrapper.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class PipelineExecutionState(Enum):
+    START = "START"
+    PROCESS = "PROCESS"
+    END = "END"
+    FAIL = "FAIL"
+    SUCCESS = "SUCCESS"
+
+
+class PipelineWrapper:
+    def __init__():
+        pass
