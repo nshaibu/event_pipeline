3fdbfc57b48791475d65c679e8c0a885cdf305c1
nafiu shaibu <nafiu@nebula.io>
2025-10-18 18:47:08 +0000

    modified profiler scripts

--- profiling/command_line_flags.py
+++ profiling/command_line_flags.py
@@ -35,5 +35,5 @@ cmd_parser.add_argument(
 )
 
 cmd_parser.add_argument(
-    "-hr", "--html_report", action="store_true", default=None, help="Generate HTML report"
+    "-r", "--html_report", action="store_true", default=None, help="Generate HTML report"
 )
